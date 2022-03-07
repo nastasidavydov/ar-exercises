92dfde18888718462c971c6d24c3d826433f3c16
Nastasi Davydov <nastasidavydov@gmail.com>
2022-03-07 23:02:00 +0000

    Find store by id and update name record of first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,16 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+
+puts @store1.name
+
+@store1.update(name: "Burberry")
+
+puts @store1.name
+
+@store2 = Store.find_by(id: 2)
+
+puts @store2.name
+
