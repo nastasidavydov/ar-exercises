d16e24fc343f2c79f687fb7c762d9a022352cb4b
Nastasi Davydov <nastasidavydov@gmail.com>
2022-03-08 00:58:30 +0000

    Add validations for Store and Employee models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+
+puts "Insert the name of the store "
+
+store_name = gets.chomp
+
+@new_store = Store.create(name: store_name)
+
+puts @new_store.errors.full_messages
